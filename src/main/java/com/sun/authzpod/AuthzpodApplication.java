3a0c0915f1791ab8eb428096828bc724c861de58
Mauro Peixoto <96122844+mauropeixotodev@users.noreply.github.com>
2024-08-25 16:02:35 -0300

    first commit

---
+++ src/main/java/com/sun/authzpod/AuthzpodApplication.java
@@ -0,0 +1,13 @@
+package com.sun.authzpod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthzpodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthzpodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sun/authzpod/AuthzpodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.authzpod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthzpodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
